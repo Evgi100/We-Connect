d3ccd503da2a243d013f25b1594b934f9dac4b6c
Lise <nakache.lise@gmail.com>
2017-10-22 22:20:00 +0300

    fixing homepage

--- public/js/app.js
+++ public/js/app.js
@@ -10,34 +10,34 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
             templateUrl: 'partial-employers.html',
             controller : "employersCtrl"
         })
-        .state('employees', {
-            url: '/employees',
-            templateUrl: '/templates/employees.html',
-            controller: 'employeeController',
-            params: { employParams: null }
-        })
-        .state('employee', {
-            url: '/employee/:id',
-            templateUrl: '/templates/employee.html',
-            controller: 'employeeController',
-            params: { employParams: null }
-        })   
-        .state('employer', {
-            url: '/employer/:id',
-            templateUrl: '/templates/employer.html',
-            controller: 'employerController',
-            params: { employerParams: null }
-        })
-        .state('projects', {
-            url: '/projects',
-            templateUrl: '/projects.html',
-            controller: 'projectsController'
-        })
-        .state('project', {
-            url: '/project/:id',
-            templateUrl: '/project.html',
-            controller: 'projectsController'
-        });
+        // .state('employees', {
+        //     url: '/employees',
+        //     templateUrl: '/templates/employees.html',
+        //     controller: 'employeeController',
+        //     params: { employParams: null }
+        // })
+        // .state('employee', {
+        //     url: '/employee/:id',
+        //     templateUrl: '/templates/employee.html',
+        //     controller: 'employeeController',
+        //     params: { employParams: null }
+        // })   
+        // .state('employer', {
+        //     url: '/employer/:id',
+        //     templateUrl: '/templates/employer.html',
+        //     controller: 'employerController',
+        //     params: { employerParams: null }
+        // })
+        // .state('projects', {
+        //     url: '/projects',
+        //     templateUrl: '/projects.html',
+        //     controller: 'projectsController'
+        // })
+        // .state('project', {
+        //     url: '/project/:id',
+        //     templateUrl: '/project.html',
+        //     controller: 'projectsController'
+        // });
     $urlRouterProvider.otherwise('/home');
 }]);
 
