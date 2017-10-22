0ff52ff2bed23189826989b46c0c927f062cc4e0
Lise <nakache.lise@gmail.com>
2017-10-22 18:36:57 +0300

    home page done

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,26 @@
-var app = angular.module('weConnectApp', ['ui-router']);
+var app = angular.module('weConnectApp', ['ui.router']);
+
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+        // .state('home', {
+        //     url: '/home',
+        //     controller: 'mainController',
+        //     templateUrl: 'home.html'
+        // })
+        .state('employers', {
+            url: '/employers',
+            templateUrl: 'js\template\employers.html',
+            // controller: 'employerController',
+            // params: { employerParams: null }
+        })
+        // .state('employer', {
+        //     url: '/employer/:id',
+        //     templateUrl: '/templates/employer.html',
+        //     controller: 'employerController',
+        //     params: { employerParams: null }
+        // })
+    })
+
+    
